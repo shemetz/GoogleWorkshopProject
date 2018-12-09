452c1c6ee4cede3d434cf82dd91298cbaf461398
eran <eranchriqui@gmail.com>
2018-12-09 12:13:33 +0200

    Server files #6
    Migrated models

--- Server/ride/models.py
+++ Server/ride/models.py
@@ -1,12 +1,14 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
-    id_ = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     facebookProfileId = models.CharField(max_length=200)
 
 class Ride(models.Model):
-    id_ = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     driverId: models.IntegerField()
     eventId: models.IntegerField()
     origin: models.CharField(max_length=200)
@@ -15,17 +17,17 @@ class Ride(models.Model):
     carModel: models.CharField(max_length=200)
     carColor: models.CharField(max_length=200)
     passengerCount: models.IntegerField()
-    extraDetails: models.textField()
+    extraDetails: models.CharField(max_length=800)
 
 class Event(models.Model):
-    id_ = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     location: models.CharField(max_length=200)
     datetime: models.DateTimeField(default=timezone.now)
     facebookEventId: models.CharField(max_length=200)
 
 class Pickup(models.Model):
-    id_: models.AutoField(primary_key=True)
+    id: models.AutoField(primary_key=True)
     userId: models.IntegerField()
     rideId: models.IntegerField()
     pickupSpot: models.CharField(max_length=200)
