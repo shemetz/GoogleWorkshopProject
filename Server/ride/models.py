ff5ade0b1f1d55968879125e026e8bc5258e7a1a
eran <eranchriqui@gmail.com>
2018-12-09 11:42:36 +0200

    Server files #4
    Added user and ride models

--- Server/ride/models.py
+++ Server/ride/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id_ = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    facebookProfileId = models.CharField(max_length=200)
+
+class Ride(models.Model):
+    id_ = models.AutoField(primary_key=True)
+    driverId: models.IntegerField()
+    eventId: models.IntegerField()
+    origin: models.CharField(max_length=200)
+    origin: models.CharField(max_length=200)
+    departureTime: models.DateTimeField(default=timezone.now)
+    carModel: models.CharField(max_length=200)
+    carColor: models.CharField(max_length=200)
+    passengerCount: models.IntegerField()
+    extraDetails: models.textField()
+
