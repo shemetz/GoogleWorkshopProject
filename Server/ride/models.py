dbe733d4ceb797b6db4b7dadef00ca5f6f32fa7e
eran <eranchriqui@gmail.com>
2018-12-09 12:01:14 +0200

    Server files #5
    Added event, pickUp and timeOfDay models

--- Server/ride/models.py
+++ Server/ride/models.py
@@ -17,3 +17,21 @@ class Ride(models.Model):
     passengerCount: models.IntegerField()
     extraDetails: models.textField()
 
+class Event(models.Model):
+    id_ = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    location: models.CharField(max_length=200)
+    datetime: models.DateTimeField(default=timezone.now)
+    facebookEventId: models.CharField(max_length=200)
+
+class Pickup(models.Model):
+    id_: models.AutoField(primary_key=True)
+    userId: models.IntegerField()
+    rideId: models.IntegerField()
+    pickupSpot: models.CharField(max_length=200)
+    pickupTime: models.DateTimeField(default=timezone.now)
+
+class TimeOfDay(models.Model):
+    hours: models.IntegerField()
+    minutes: models.IntegerField()
+
