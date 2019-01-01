4a554d2a649d636e5284cf63e18bc40297de82ce
itamar <itamar@feezback.com>
2019-01-02 00:29:52 +0200

    Fix launcher bug

--- app/src/main/java/org/team2/ridetogather/WelcomeActivity.java
+++ app/src/main/java/org/team2/ridetogather/WelcomeActivity.java
@@ -3,18 +3,13 @@ package org.team2.ridetogather;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.media.FaceDetector;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.*;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -38,6 +33,7 @@ public class WelcomeActivity extends AppCompatActivity {
         if (!prefManager.isFirstTimeLaunch()) {
             launchHomeScreen();
             finish();
+            return;
         }
 
         // Making notification bar transparent
