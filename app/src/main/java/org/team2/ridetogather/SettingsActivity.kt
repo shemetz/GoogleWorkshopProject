5b59a34c56d0493b47758684b799f79c92ff967c
itamar <itamar@feezback.com>
2019-02-22 16:38:03 +0200

    Remove temporary hack and unneeded function in settings

--- app/src/main/java/org/team2/ridetogather/SettingsActivity.kt
+++ app/src/main/java/org/team2/ridetogather/SettingsActivity.kt
@@ -2,10 +2,8 @@ package org.team2.ridetogather
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 import android.util.Log
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_settings.*
 
 /**
@@ -29,10 +27,6 @@ class SettingsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         Log.d(tag, "Created $tag")
 
-        // small temporary hack to allow revisiting the welcome screen
-        val prefManager = PrefManager(this)
-        prefManager.isFirstTimeLaunch = true
-
         // android.R.id.content is probably for old style activity
         supportFragmentManager.beginTransaction()
                 // .replace(android.R.id.content, SettingsFragment())
@@ -44,21 +38,6 @@ class SettingsActivity : AppCompatActivity() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.pref_settings)
-        }
-
-        override fun onPreferenceTreeClick(preference: Preference): Boolean {
-            return when (preference.key) {
-                getString(R.string.pref_key_change_password) -> {
-                    //example
-                    Toast.makeText(context, "You clicked this button!", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> {
-                    //example
-                    Toast.makeText(context, "You clicked another button!", Toast.LENGTH_SHORT).show()
-                    super.onPreferenceTreeClick(preference)
-                }
-            }
         }
     }
 }
